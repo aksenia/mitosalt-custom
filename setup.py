932f80890515f0ebbb0e85f27dfd0c6e671a55ab
Ksenia Lavrichenko <ksenia.lavrichenko@gmail.com>
2025-10-02 15:51:54 +0200

    saltshaker package

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from pathlib import Path
+
+with Path("README.md").open(encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saltshaker",
+    version="0.1.0",
+    author="OUS AMG",
+    description="Pattern classification and visualization for MitoSAlt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aksenia/mitosalt_custom",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pandas>=1.3.0",
+        "numpy>=1.20.0",
+        "matplotlib>=3.3.0",
+        "biopython>=1.78",
+    ],
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "saltshaker=saltshaker.__main__:main",
+        ],
+    },
+    python_requires=">=3.8",
+)
